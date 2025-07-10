3dce1ce693169aacde7397dfcac137b1d3a573a8
Danielsasa <danielsasa1@hotmail.com>
2025-07-09 19:42:11 -0600

    💄 se agregaron estilos y correction del header

--- blog-frontend/src/components/Header/Header.jsx
+++ blog-frontend/src/components/Header/Header.jsx
@@ -1,42 +1,51 @@
-import React from 'react'
-import './Header.css'
 import { NavLink } from 'react-router-dom'
+import './Header.css'
 
 const Header = () => {
   const menuItems = [
     { id: 'inicio', label: 'Inicio', path: '/' },
-    { id: 'nuevo-post', label: 'Escribir', path: '/newpost' },
-    { id: 'contacto', label: 'Contacto', path: '/contact' },
+    { id: 'newpost', label: 'Escribir', path: '/newpost' },
+    { id: 'contacto', label: 'Contacto', path: '/contact' }
   ]
 
   return (
     <header className='header'>
       <div className='header-container'>
+
         <div className='header-left'>
           <div className='logo'>
-            <span className='logo-text'>Blog de Cesar Guerra</span>
-            <nav className='nav-menu'>
-              {menuItems.map(item => (
-                <NavLink
-                  key={item.id}
-                  to={item.path}
-                  className={({ isActive }) => (isActive ? 'active' : '')}
-                >
-                  {item.label}
-                </NavLink>
-              )
-              )}
-            </nav>
-
+            <span className='logo-text'>Blog de César Guerra</span>
           </div>
+          <nav className='nav-menu'>
+            {menuItems.map((item) => (
+              <NavLink
+                key={item.id}
+                to={item.path}
+                className={({ isActive }) => `nav-item ${isActive ? 'active' : ''}`}
+              >
+                {item.label}
+              </NavLink>
+            ))}
+          </nav>
         </div>
 
         <div className='header-right'>
-          *
+          <NavLink to='/login' className='nav-item'> Iniciar sesión</NavLink>
+          <NavLink to='/register' className='nav-item'> Registrarse</NavLink>
+          <span
+            className='nav-item'
+            onClick={() => {
+            // Aquí va la lógica para cerrar sesión
+              console.log('Haz cerrado la sesión')
+            }}
+          > Cerrar sesión
+          </span>
+          <div className='user-profile'>
+            <span className='user-greeting'>Hola, Usuario</span>
+          </div>
         </div>
       </div>
     </header>
   )
 }
-
 export default Header
