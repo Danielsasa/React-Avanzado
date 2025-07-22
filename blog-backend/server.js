ab5072274627afa87953ecffd3fac7e6ffeee5d1
Danielsasa <danielsasa1@hotmail.com>
2025-07-21 20:04:29 -0600

    ✨ Get one post with params

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -19,6 +19,21 @@ app.get('/api/v1/posts', (req, res) => {
   res.json(postsData)
 })
 
+//Params
+//Un params sirve para hacer una ruta dinamica, Por ejemp;o si quiero traer la informacion de un post en especifico, puedo hacer que una ruta reciba el id del post y me regrese la informacion de ese post 
+// Param:(/api/v1/posts/:postId)
+app.get('/api/v1/posts/:postId', (req, res) => {
+  // const postId = req.params.postId
+  const { postId } = req.params // Desestructuración de objetos
+  console.log(`Recibí una petición GET en la ruta /api/v1/posts/${postId}`)
+  const post = postsData.find((post) => post.id === parseInt(postId))
+  if (post) {
+    res.json(post)
+  } else {
+    res.status(404).json({ error: 'Post not found' })
+  }
+})
+
 // #4 starting the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
