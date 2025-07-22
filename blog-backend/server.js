ec472bdc1c99ae3f3ad1e822cfe3412837ab0a09
Danielsasa <danielsasa1@hotmail.com>
2025-07-21 19:47:23 -0600

    ✨ add getAlposts basic endpoint

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -1,5 +1,6 @@
 // #1 importing the express module cambio de puert $env:port=4000; node server.js
 import express from 'express'
+import postsData from './data/posts.json' with { type: 'json'}
 // #2acreating an instance of express
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,6 +14,11 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('Saludos G40!')
 })
+
+app.get('/api/v1/posts', (req, res) => {
+  res.json(postsData)
+})
+
 // #4 starting the server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
